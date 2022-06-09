e8721673c763d964a743e4fb0d9d8a1c7034c2c1
Arif <arifmmmdov0@gmail.com>
2022-06-09 09:53:30 +0400

    The best app ever

---
+++ GamingApp/Main.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace GamingApp
+{
+    partial class DinoGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DinoGame));
+            this.lbl_score = new System.Windows.Forms.Label();
+            this.lbl_time = new System.Windows.Forms.Label();
+            this.MyTimer = new System.Windows.Forms.Timer(this.components);
+            this.lbl_RestartGame = new System.Windows.Forms.Label();
+            this.TimerForSearchingRealTime = new System.Windows.Forms.Timer(this.components);
+            this.lbl_SpeedOfDino = new System.Windows.Forms.Label();
+            this.pb_speedOfDino = new System.Windows.Forms.ProgressBar();
+            this.pb_ground = new System.Windows.Forms.PictureBox();
+            this.pb_lastChristmasTree = new System.Windows.Forms.PictureBox();
+            this.pb_thinTree = new System.Windows.Forms.PictureBox();
+            this.PB_TRex = new System.Windows.Forms.PictureBox();
+            this.pb_santa = new System.Windows.Forms.PictureBox();
+            this.PB_MiddleChristmasTree = new System.Windows.Forms.PictureBox();
+            this.PB_SmallChristmasTree = new System.Windows.Forms.PictureBox();
+            this.pb_atmosphere2 = new System.Windows.Forms.PictureBox();
+            this.pb_atmosphere1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_lastChristmasTree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_thinTree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_TRex)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_santa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_MiddleChristmasTree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_SmallChristmasTree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_atmosphere2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_atmosphere1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_score
+            // 
+            resources.ApplyResources(this.lbl_score, "lbl_score");
+            this.lbl_score.Name = "lbl_score";
+            // 
+            // lbl_time
+            // 
+            resources.ApplyResources(this.lbl_time, "lbl_time");
+            this.lbl_time.Name = "lbl_time";
+            // 
+            // MyTimer
+            // 
+            this.MyTimer.Tick += new System.EventHandler(this.MyTimer_Tick);
+            // 
+            // lbl_RestartGame
+            // 
+            resources.ApplyResources(this.lbl_RestartGame, "lbl_RestartGame");
+            this.lbl_RestartGame.Name = "lbl_RestartGame";
+            // 
+            // TimerForSearchingRealTime
+            // 
+            this.TimerForSearchingRealTime.Interval = 1000;
+            this.TimerForSearchingRealTime.Tick += new System.EventHandler(this.TimerForSearchingRealTime_Tick);
+            // 
+            // lbl_SpeedOfDino
+            // 
+            resources.ApplyResources(this.lbl_SpeedOfDino, "lbl_SpeedOfDino");
+            this.lbl_SpeedOfDino.Name = "lbl_SpeedOfDino";
+            // 
+            // pb_speedOfDino
+            // 
+            resources.ApplyResources(this.pb_speedOfDino, "pb_speedOfDino");
+            this.pb_speedOfDino.Name = "pb_speedOfDino";
+            // 
+            // pb_ground
+            // 
+            resources.ApplyResources(this.pb_ground, "pb_ground");
+            this.pb_ground.Name = "pb_ground";
+            this.pb_ground.TabStop = false;
+            // 
+            // pb_lastChristmasTree
+            // 
+            this.pb_lastChristmasTree.BackColor = System.Drawing.SystemColors.Control;
+            this.pb_lastChristmasTree.BackgroundImage = global::GamingApp.Properties.Resources.Snowing1;
+            resources.ApplyResources(this.pb_lastChristmasTree, "pb_lastChristmasTree");
+            this.pb_lastChristmasTree.Image = global::GamingApp.Properties.Resources.imgbin_christmas_tree_gift_new_year_png;
+            this.pb_lastChristmasTree.Name = "pb_lastChristmasTree";
+            this.pb_lastChristmasTree.TabStop = false;
+            this.pb_lastChristmasTree.Tag = "Tree";
+            // 
+            // pb_thinTree
+            // 
+            this.pb_thinTree.BackgroundImage = global::GamingApp.Properties.Resources.Snowing1;
+            resources.ApplyResources(this.pb_thinTree, "pb_thinTree");
+            this.pb_thinTree.Image = global::GamingApp.Properties.Resources.newYearPNG222;
+            this.pb_thinTree.Name = "pb_thinTree";
+            this.pb_thinTree.TabStop = false;
+            this.pb_thinTree.Tag = "Tree";
+            // 
+            // PB_TRex
+            // 
+            this.PB_TRex.BackColor = System.Drawing.Color.White;
+            this.PB_TRex.BackgroundImage = global::GamingApp.Properties.Resources.Snowing1;
+            resources.ApplyResources(this.PB_TRex, "PB_TRex");
+            this.PB_TRex.Image = global::GamingApp.Properties.Resources.MyTirexFlying;
+            this.PB_TRex.Name = "PB_TRex";
+            this.PB_TRex.TabStop = false;
+            // 
+            // pb_santa
+            // 
+            this.pb_santa.BackgroundImage = global::GamingApp.Properties.Resources.Snowing1;
+            resources.ApplyResources(this.pb_santa, "pb_santa");
+            this.pb_santa.Name = "pb_santa";
+            this.pb_santa.TabStop = false;
+            this.pb_santa.Tag = "Santa";
+            // 
+            // PB_MiddleChristmasTree
+            // 
+            this.PB_MiddleChristmasTree.BackColor = System.Drawing.SystemColors.Control;
+            this.PB_MiddleChristmasTree.BackgroundImage = global::GamingApp.Properties.Resources.Snowing1;
+            resources.ApplyResources(this.PB_MiddleChristmasTree, "PB_MiddleChristmasTree");
+            this.PB_MiddleChristmasTree.Image = global::GamingApp.Properties.Resources._95820_christmas_tree_new_year_decoration_for_holiday_2020;
+            this.PB_MiddleChristmasTree.Name = "PB_MiddleChristmasTree";
+            this.PB_MiddleChristmasTree.TabStop = false;
+            this.PB_MiddleChristmasTree.Tag = "Tree";
+            // 
+            // PB_SmallChristmasTree
+            // 
+            this.PB_SmallChristmasTree.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.PB_SmallChristmasTree.BackgroundImage = global::GamingApp.Properties.Resources.Snowing1;
+            resources.ApplyResources(this.PB_SmallChristmasTree, "PB_SmallChristmasTree");
+            this.PB_SmallChristmasTree.Image = global::GamingApp.Properties.Resources.newYearPNG;
+            this.PB_SmallChristmasTree.Name = "PB_SmallChristmasTree";
+            this.PB_SmallChristmasTree.TabStop = false;
+            this.PB_SmallChristmasTree.Tag = "Tree";
+            // 
+            // pb_atmosphere2
+            // 
+            resources.ApplyResources(this.pb_atmosphere2, "pb_atmosphere2");
+            this.pb_atmosphere2.Name = "pb_atmosphere2";
+            this.pb_atmosphere2.TabStop = false;
+            // 
+            // pb_atmosphere1
+            // 
+            resources.ApplyResources(this.pb_atmosphere1, "pb_atmosphere1");
+            this.pb_atmosphere1.Name = "pb_atmosphere1";
+            this.pb_atmosphere1.TabStop = false;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // DinoGame
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_time);
+            this.Controls.Add(this.lbl_score);
+            this.Controls.Add(this.lbl_SpeedOfDino);
+            this.Controls.Add(this.pb_speedOfDino);
+            this.Controls.Add(this.pb_ground);
+            this.Controls.Add(this.pb_lastChristmasTree);
+            this.Controls.Add(this.pb_thinTree);
+            this.Controls.Add(this.PB_TRex);
+            this.Controls.Add(this.lbl_RestartGame);
+            this.Controls.Add(this.pb_santa);
+            this.Controls.Add(this.PB_MiddleChristmasTree);
+            this.Controls.Add(this.PB_SmallChristmasTree);
+            this.Controls.Add(this.pb_atmosphere2);
+            this.Controls.Add(this.pb_atmosphere1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DinoGame";
+            this.Load += new System.EventHandler(this.DinoGame_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.WhenKeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.WhenKeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_lastChristmasTree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_thinTree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_TRex)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_santa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_MiddleChristmasTree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_SmallChristmasTree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_atmosphere2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_atmosphere1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox PB_MiddleChristmasTree;
+        private System.Windows.Forms.PictureBox PB_SmallChristmasTree;
+        private System.Windows.Forms.Label lbl_score;
+        private System.Windows.Forms.Label lbl_time;
+        private System.Windows.Forms.Timer MyTimer;
+        private System.Windows.Forms.PictureBox pb_atmosphere1;
+        private System.Windows.Forms.Label lbl_RestartGame;
+        private System.Windows.Forms.Timer TimerForSearchingRealTime;
+        private System.Windows.Forms.PictureBox pb_atmosphere2;
+        private System.Windows.Forms.PictureBox pb_ground;
+        private System.Windows.Forms.PictureBox PB_TRex;
+        private System.Windows.Forms.PictureBox pb_santa;
+        private System.Windows.Forms.Label lbl_SpeedOfDino;
+        private System.Windows.Forms.ProgressBar pb_speedOfDino;
+        private System.Windows.Forms.PictureBox pb_thinTree;
+        private System.Windows.Forms.PictureBox pb_lastChristmasTree;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GamingApp/Main.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Media;
+
+
+namespace GamingApp
+{
+    public partial class DinoGame : Form
+    {
+        bool jumping;
+        bool increaseSpeed;
+        bool decreaseSpeed;
+        int jumpSpeed;
+        int force;
+        int score,time;
+        int SpeedOfDino;
+        bool isGameFail;
+        int n = 0;
+       
+        Random random = new Random();
+        int position;
+        
+
+        public DinoGame()
+        {
+            InitializeComponent();
+            StartGame();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void StartGame()
+        {
+            force = 20;
+            jumpSpeed = 0;
+            increaseSpeed = false;
+            decreaseSpeed = false;
+            isGameFail = false;
+            jumping = false;
+            SpeedOfDino = 10;
+            score = 0;
+            time = 0;
+            PB_TRex.Image = Properties.Resources.running_1_;
+            lbl_score.Text = "Score: " + score;
+
+            PB_TRex.Top = 243;
+            n = 50;
+            foreach(Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "Tree")
+                {
+                    
+                    x.Left = this.ClientSize.Width  + n;
+                    n = n + random.Next(50,200);
+                    
+                }
+
+
+            }
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "Santa")
+                {
+
+                    if (x is PictureBox && (string)x.Tag == "Santa")
+                    {
+
+                        x.Left = this.ClientSize.Width +random.Next(100, 500) ;
+
+                    }
+                }
+            }
+            TimerForSearchingRealTime.Start();
+            MyTimer.Start();
+
+        }
+        int pushing = 1;
+
+        private void WhenKeyIsDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Keys.Space )
+            {
+                if (isGameFail == true && pushing==0)
+                {
+                    StartGame();
+                }
+                pushing = 1;
+                if (jumping == false)
+                {
+                    jumping = true;
+                }
+                PB_TRex.Image = Properties.Resources.MyTirexFlying;
+                
+             
+            }
+            
+            if (e.KeyCode == Keys.Right&&SpeedOfDino<30)
+            {
+                if(SpeedOfDino<30)
+                increaseSpeed = true;
+
+            }
+            if (e.KeyCode == Keys.Left&&SpeedOfDino>5)
+            {
+                if(SpeedOfDino>6)
+                decreaseSpeed = true;
+            }
+            if (e.KeyCode == Keys.R&&isGameFail==true)
+            {
+                lbl_RestartGame.Text = "The game restarted!";
+                
+                StartGame();
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                if(jumping==false)
+                jumping = true;
+
+                if (PB_TRex.Top < 243
+                    && isGameFail == false)
+                {
+                    PB_TRex.Image = Properties.Resources.MyTirexFlying;
+                }
+            }
+            
+
+            
+        }
+
+        private void WhenKeyIsUp(object sender, KeyEventArgs e)
+        {
+
+
+            if (e.KeyCode == Keys.Space)
+            {
+                if (jumping == true)
+                {
+                    jumping = false;
+                }
+                pushing = 0;
+
+
+            }
+            if (e.KeyCode == Keys.Right && SpeedOfDino < 30)
+            {
+                increaseSpeed = false;
+
+            }
+            if (e.KeyCode == Keys.Left && SpeedOfDino > 5)
+            {
+                decreaseSpeed = false;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                if (jumping == true)
+                {
+                    jumping = false;
+                }
+
+
+            }
+
+        }
+
+        private void MyTimer_Tick(object sender, EventArgs e)
+        {
+            PB_TRex.Top += jumpSpeed;
+            lbl_score.Text = "Score: " + score;
+            if (jumping == true && force < 0)
+            {
+                jumping = false;
+
+            }
+            if (SpeedOfDino > 30) increaseSpeed = false;
+            if (SpeedOfDino < 6) decreaseSpeed = false;
+            if (increaseSpeed) SpeedOfDino++;
+            if (decreaseSpeed) SpeedOfDino-=2;
+
+            pb_speedOfDino.Value = SpeedOfDino*3;
+
+            if (jumping == true)
+            {
+                jumpSpeed = -12;
+
+                force = force - 1;
+            }
+            else
+            {
+                jumpSpeed = 12;
+
+            }
+
+            if(PB_TRex.Top>243 && jumping == false)
+            {
+                force = 20;
+                PB_TRex.Top = 243;
+                jumpSpeed = 0;
+                PB_TRex.Image = Properties.Resources.running_1_;
+            }
+            if (n > 870)
+            {
+                n = 50;
+
+            }
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox && (string)x.Tag == "Tree")
+                {
+                    
+                    x.Left = x.Left - SpeedOfDino;
+                    if (x.Left < -100)
+                    {
+                        n = n + random.Next(50,200);
+                        x.Left = this.ClientSize.Width + n;
+                        score++;
+                    }
+                    
+                    if (PB_TRex.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        MyTimer.Stop();
+                        TimerForSearchingRealTime.Stop();
+                        lbl_RestartGame.Visible = true;
+                        lbl_RestartGame.Text = "Push R to restart the game!";
+                        PB_TRex.Image = Properties.Resources.dead_1_;
+                        isGameFail = true;
+                    }
+                }
+            }
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox && (string)x.Tag == "Santa")
+                {
+                    
+                    x.Left = x.Left - SpeedOfDino-10;
+                    if (x.Left < -100&&isGameFail==false)
+                    {
+                        x.Left = this.ClientSize.Width + random.Next(500,1000);
+                    }
+                    
+                    if (PB_TRex.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        MyTimer.Stop();
+                        TimerForSearchingRealTime.Stop();
+                        lbl_RestartGame.Visible = true;
+                        lbl_RestartGame.Text = "Push R to restart the game!";
+                        PB_TRex.Image = Properties.Resources.dead_1_;
+                        isGameFail = true;
+                    }
+                }
+            }
+
+            
+
+        }
+
+        private void TimerForSearchingRealTime_Tick(object sender, EventArgs e)
+        {
+            time++;
+            lbl_time.Text = "Time: " + time;
+            if (time == 3)
+            {
+                lbl_RestartGame.Visible = false;
+            }
+        }
+
+        private void DinoGame_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            string MessageToUser = "   This game is developed for fun. You can increase and decrease speed of T-Rex unlike other Dino games.\n" +
+                "   I didn't have any equipments for changing the color of T-Rexes.So, they could be seen different.\n" +
+                "   PictureBoxes are not the same size with pictures inside of them,and the PictureBoxes couldn't be cut. So, the T-Rex could touch the border,and you could fail the game for this.\n" +
+                "   I didn't find any gif to add ground side,but as soon as I find, I will update the game.\n" +
+                "            Happy New Year from Arif Mammadov";
+            MessageBox.Show(MessageToUser,"Dino Game");
+
+
+        }
+
+       
+private void brn_aboutTheGame_Click(object sender, EventArgs e)
+{
+   
+}
+
+        private void PB_TRex_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
